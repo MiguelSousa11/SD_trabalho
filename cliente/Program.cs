eef61d061b0bfc1f6d99daa94ded459873a7946c
Alexandre <al76741@alunos.utad.pt>
2024-04-03 20:28:45 +0100

    Código do cliente

--- cliente/Program.cs
+++ cliente/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+public class Cliente
+{
+	public static void Main(string[] args)
+	{
+		string server = "127.0.0.1"; // Endereço IP do servidor
+		int port = 8888; // Porta do servidor
+
+		TcpClient client = new TcpClient(server, port);
+		StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+		StreamReader reader = new StreamReader(client.GetStream());
+
+		Console.WriteLine("Conectado ao servidor. Enviando ID...");
+		writer.WriteLine("ID 123"); // Exemplo de ID
+
+		// Loop para interação com o servidor, pode ser encerrado com "QUIT"
+		string input = "";
+		while (input.ToUpper() != "QUIT")
+		{
+			input = Console.ReadLine();
+			writer.WriteLine(input);
+			if (input.ToUpper() == "QUIT")
+			{
+				Console.WriteLine(reader.ReadLine());
+				break;
+			}
+		}
+
+		client.Close();
+	}
+}
